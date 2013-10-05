9cbb5cfcc2c67329fa0902874911dfc01db0eeed
Alex Reis <alexeshadow@gmail.com>
2013-10-05 11:38:47 +0200

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtsyradarRails::Application.config.secret_key_base = '944e5e6181c901184283d8c14d25bc85c569e7c6c13d865543d01840e5149639288f79046417fa012f0843261adf89ccabafbba4b1f8d60a15a2d8f3a853a6d2'
