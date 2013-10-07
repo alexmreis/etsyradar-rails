9d3bfb0e1a4c761d8751f0ce4b313a3dd305b874
Alex Reis <alexeshadow@gmail.com>
2013-10-08 00:24:40 +0200

    Using sendgrid on heroku

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -1,13 +1,30 @@
-Pony.options = {
-  :from => ENV['GMAIL_USER'],
-  :via => :smtp,
-  :via_options => {
-    :address              => 'smtp.gmail.com',
-    :port                 => '587',
-    :enable_starttls_auto => true,
-    :user_name            => ENV['GMAIL_USER'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain               => "etsyradar.herokuapp.com" # the HELO domain provided by the client to the server
+if Rails.env.development? or Rails.env.test?
+  Pony.options = {
+    :from => ENV['GMAIL_USER'],
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['GMAIL_USER'],
+      :password             => ENV['GMAIL_PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "etsyradar.herokuapp.com" # the HELO domain provided by the client to the server
+    }
   }
-}
+end
+
+if Rails.env.production?
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  } 
+end
